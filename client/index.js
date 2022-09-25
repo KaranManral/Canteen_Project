459da597d588b4f6057869fabb274fc3fd456e73
KaranManral <karanmnaral2000@gmail.com>
2022-09-25 14:41:05 +0530

    Redirect Fixed

--- client/index.js
+++ client/index.js
@@ -1,42 +1,34 @@
-$("document").ready(()=>{
-    if(Date.now()-parseInt(sessionStorage.getItem("visited"))>180000)
-    {    
-        $("#first-page").css("display","block");
-        $("#HomePage").css("display","none");
-        sessionStorage.setItem("visited",Date.now());
+$("document").ready(() => {
+    if (Date.now() - parseInt(sessionStorage.getItem("visited")) > 180000) {
+        $("#first-page").css("display", "block");
+        $("#HomePage").css("display", "none");
+        sessionStorage.setItem("visited", Date.now());
     }
-    else if(parseInt(sessionStorage.getItem("count"))>1)
-    {    
-        $("#first-page").css("display","none");
-        $("#HomePage").css("display","block");
+    else if (parseInt(sessionStorage.getItem("count")) > 1) {
+        $("#first-page").css("display", "none");
+        $("#HomePage").css("display", "block");
     }
-    $("#explorer").click(()=>{
-        $(".front-img").animate({top:"-200%"},1500,()=>{
-            $(".front-img").css("display","none");
+    $("#explorer").click(() => {
+        $(".front-img").animate({ top: "-200%" }, 1500, () => {
+            $(".front-img").css("display", "none");
         });
-        $("#explorer").animate({top:"100%"},400,()=>{
-            $("#explorer").css("display","none");
+        $("#explorer").animate({ top: "100%" }, 400, () => {
+            $("#explorer").css("display", "none");
         });
         $("#HomePage").fadeIn(1500);
     });
-    $("#first-page").on("mouseenter",()=>{
-        $(".front-img").fadeTo(0,0.5);
-        $("#explorer").animate({top:"64%"},500);
+    $("#first-page").on("mouseenter", () => {
+        $(".front-img").fadeTo(0, 0.5);
+        $("#explorer").animate({ top: "64%" }, 500);
         $("#first-page").stop();
-    }).on("mouseleave",()=>{
-        $(".front-img").fadeTo(0,1);
-        $("#explorer").animate({top:"100%"},500);
+    }).on("mouseleave", () => {
+        $(".front-img").fadeTo(0, 1);
+        $("#explorer").animate({ top: "100%" }, 500);
         $("#first-page").stop();
     })
-    $(".front-img").on("click",()=>{
-        $(".front-img").fadeTo(0,0.5);
-        $("#explorer").animate({top:"64%"},500);
+    $(".front-img").on("click", () => {
+        $(".front-img").fadeTo(0, 0.5);
+        $("#explorer").animate({ top: "64%" }, 500);
         $("#first-page").stop();
     })
 });
-
-function setActiveLink(param) {
-    let currentActive = document.getElementsByClassName("active");
-    if (currentActive.length > 0) currentActive[0].classList.remove("active");
-    param.target.classList.add("active");
-  }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,56 @@
 const express = require('express');
 const path = require("path");
+const fs = require("fs");
 const app = express();
 
-app.use('/',express.static(path.join(__dirname,"../client")));
+let readData = "";
+
+app.use('/', express.static(path.join(__dirname, "../client")));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/popper.js/dist/umd')));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font')));
 
+fs.readFile(path.join(__dirname, "../client/index.html"), "utf-8", (err, data) => {
+  if (err) {
+    return console.log("Error");
+  }
+  readData = data;
+})
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"../client/index.html"));
+  res.send(readData);
+});
+
+app.get('/menu', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./MenuPage.html");
+  res.send(temp);
+});
+
+app.get('/login', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./LoginPage.html");
+  res.send(temp);
+});
+
+app.get('/support', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./SupportPage.html");
+  res.send(temp);
+});
+
+app.get('/feedback', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./FeedbackPage.html");
+  res.send(temp);
+});
+
+app.get('/register', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./RegisterPage.html");
+  res.send(temp);
+});
+
+app.get('/about', (req, res) => {
+  let temp = readData.replace("./HomePage.html", "./AboutPage.html");
+  res.send(temp);
 });
 
 app.listen(3000, () => {
