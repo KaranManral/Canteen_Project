b287ce0cac6679793c4c14ebb2c319b58adf0533
KaranManral <karanmnaral2000@gmail.com>
2022-10-04 12:38:35 +0530

    Home Page Done

--- client/assets/js/validate.js
+++ client/assets/js/validate.js
@@ -1,37 +1,44 @@
 
 window.addEventListener("load", () => {
+    $("#email").on("focus", () => {
+        $("#form-message").html("");
+    });
     $("#form").submit((event) => {
         event.preventDefault();
+        $("#email").removeClass("shaker border border-danger");
+        $("#pwd").removeClass("shaker border border-danger");
+        $("#msg").text("");
         let email = $("#email").val();
         let emailParts = email.split("@");
         if (emailParts.length > 2 || email.length < 4 || email.length > 64) {
-            $(".text-danger").text("Invalid Email");
+            $("#msg").text("Invalid Email");
             $("#email").addClass("shaker border border-danger").on("focus", () => {
                 $("#email").removeClass("shaker border border-danger");
-                $(".text-danger").text("");
+                $("#msg").text("");
             });
             return;
         }
         else if (emailParts[1] != "keshav.du.ac.in") {
-            $(".text-danger").text("Invalid Email");
+            $("#msg").text("Invalid Email");
             $("#email").addClass("shaker border border-danger").on("focus", () => {
                 $("#email").removeClass("shaker border border-danger");
-                $(".text-danger").text("");
+                $("#msg").text("");
             });
             return;
         }
         else {
             let pwd = $("#pwd").val();
-            let reg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d@*_]{7,19}$/;
-            if (reg.test(pwd) === false) {
-                $(".text-danger").text("Invalid Password");
+            if (pwd.length < 8 || pwd.length > 20) {
+                $("#msg").text("Invalid Password");
                 $("#pwd").addClass("shaker border border-danger").on("focus", () => {
                     $("#pwd").removeClass("shaker border border-danger");
-                    $(".text-danger").text("");
+                    $("#msg").text("");
                 });
                 return;
             }
             else {
+                $("#login-container").slideUp(500);
+                $("#load-container").slideDown(500);
                 $.ajax({
                     url: "/login",
                     type: "post",
@@ -39,10 +46,197 @@ window.addEventListener("load", () => {
                     async: 'false',
                     data: $("#form").serialize(),
                     success: function (data) {
-                        alert("success");
+                        if (data.response === 100)
+                            setTimeout(() => {
+                                $("#spin").fadeOut(200);
+                                $("#tick").fadeIn(200);
+                                sessionStorage.setItem("count", 2);
+                                sessionStorage.setItem("visited", Date.now());
+                                setTimeout(() => {
+                                    window.location.href = "/";
+                                }, 1000);
+                            }, 2000);
+                        if (data.response === 200)
+                            setTimeout(() => {
+                                $("#spin").fadeOut(200);
+                                $("#tick").fadeIn(200);
+                                sessionStorage.setItem("count", 2);
+                                sessionStorage.setItem("visited", Date.now());
+                                setTimeout(() => {
+                                    window.location.href = "/";
+                                }, 1000);
+                            }, 2000);
+                        if (data.response === 500) {
+                            setTimeout(() => {
+                                $("#form-message").text("Failed to Login, Try Again.");
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                                setTimeout(() => {
+                                    $("#msg").text("Wrong Password");
+                                    $("#pwd").addClass("shaker border border-danger").on("focus", () => {
+                                        $("#pwd").removeClass("shaker border border-danger");
+                                        $("#msg").text("");
+                                    });
+                                }, 500);
+                            }, 2000);
+                        }
+                        if (data.response === 404) {
+                            setTimeout(() => {
+                                $("#form-message").text("Email not registered. Use another or create one.");
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                                setTimeout(() => {
+                                    $("#msg").text("Wrong Email");
+                                    $("#email").addClass("shaker border border-danger").on("focus", () => {
+                                        $("#email").removeClass("shaker border border-danger");
+                                        $("#msg").text("");
+                                    });
+                                }, 500);
+                            }, 2000);
+                        }
+                        if (data.response === 400) {
+                            setTimeout(() => {
+                                $("#form-message").text("Invalid data entries in login form.");
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                                setTimeout(() => {
+                                    $("#msg").text("Invalid Email or Password");
+                                    $("#email").addClass("shaker border border-danger").on("focus", () => {
+                                        $("#email").removeClass("shaker border border-danger");
+                                        $("#msg").text("");
+                                    });
+                                }, 500);
+                            }, 2000);
+                        }
                     },
                     error: function (jqXHR, textStatus, err) {
-                        alert('text status ' + textStatus + ', err ' + err);
+                        window.location.reload();
+                    }
+                })
+            }
+        }
+    })
+
+    $("#regForm").submit((event) => {
+        event.preventDefault();
+        $("#email").removeClass("shaker border border-danger");
+        $("#pwd").removeClass("shaker border border-danger");
+        $("#msg").text("");
+        let fname = $("#fname").val();
+        let lname = $("#lname").val();
+        let fnameTest = /^(?=.*[A-Za-z])[A-Za-z]{3,64}$/
+        let lnameTest = /^(?=.*[A-Za-z])[A-Za-z]{3,64}$/
+        if (fnameTest.test(fname) === false) {
+            $("#msg").text("Invalid First Name");
+            $("#fname").addClass("shaker border border-danger").on("focus", () => {
+                $("#fname").removeClass("shaker border border-danger");
+                $("#msg").text("");
+            });
+            return;
+        }
+        if (lname.length > 0 && lnameTest.test(lname) === false) {
+            $("#msg").text("Invalid Last Name");
+            $("#lname").addClass("shaker border border-danger").on("focus", () => {
+                $("#lname").removeClass("shaker border border-danger");
+                $("#msg").text("");
+            });
+            return;
+        }
+        let email = $("#email").val();
+        let emailParts = email.split("@");
+        if (emailParts.length > 2 || email.length < 4 || email.length > 64) {
+            $("#msg").text("Invalid Email");
+            $("#email").addClass("shaker border border-danger").on("focus", () => {
+                $("#email").removeClass("shaker border border-danger");
+                $("#msg").text("");
+            });
+            return;
+        }
+        else if (emailParts[1] != "keshav.du.ac.in") {
+            $("#msg").text("Invalid Email");
+            $("#email").addClass("shaker border border-danger").on("focus", () => {
+                $("#email").removeClass("shaker border border-danger");
+                $("#msg").text("");
+            });
+            return;
+        }
+        else {
+            let pwd = $("#rpwd").val();
+            let cpwd = $("#rcpwd").val();
+            let reg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d@*_]{7,19}$/;
+            if (reg.test(pwd) === false) {
+                $("#msg").text("Invalid Password");
+                $("#rpwd").addClass("shaker border border-danger").on("focus", () => {
+                    $("#rpwd").removeClass("shaker border border-danger");
+                    $("#msg").text("");
+                });
+                return;
+            }
+            else if (pwd !== cpwd) {
+                $("#msg").text("Passwords do not match");
+                $("#rcpwd").addClass("shaker border border-danger").on("focus", () => {
+                    $("#rcpwd").removeClass("shaker border border-danger");
+                    $("#msg").text("");
+                });
+                return;
+            }
+            else {
+                $("#login-container").slideUp(500);
+                $("#load-container").slideDown(500);
+                $.ajax({
+                    url: "/register",
+                    type: "post",
+                    dataType: "json",
+                    async: 'false',
+                    data: $("#regForm").serialize(),
+                    success: function (data) {
+                        if (data.response === 100) {
+                            setTimeout(() => {
+                                $("#spin").fadeOut(200);
+                                $("#tick").fadeIn(200);
+                                sessionStorage.setItem("count", 2);
+                                sessionStorage.setItem("visited", Date.now());
+                                setTimeout(() => {
+                                    window.location.href = "/";
+                                }, 1000);
+                            }, 2000);
+                        }
+                        if (data.response === 200) {
+                            setTimeout(() => {
+                                $("#spin").fadeOut(200);
+                                $("#tick").fadeIn(200);
+                                setTimeout(() => {
+                                    window.location.href = "/login";
+                                }, 1500);
+                            }, 2000);
+                        }
+                        if (data.response === 500) {
+                            setTimeout(() => {
+                                $("#form-message").text("Failed to Register. Try Again in some time.");
+                                $("#regForm").trigger('reset');
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                            }, 2000);
+                        }
+                        if (data.response === 1) {
+                            setTimeout(() => {
+                                $("#form-message").html("Username already taken. Continue to <a class='text-danger' href='/login'><u>Login</u></a> or use another email.");
+                                $("#regForm").trigger('reset');
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                            }, 2000);
+                        }
+                        if (data.response === 400) {
+                            setTimeout(() => {
+                                $("#form-message").text("Invalid data entries in registration form.");
+                                $("#regForm").trigger('reset');
+                                $("#load-container").slideUp(500);
+                                $("#login-container").slideDown(500);
+                            }, 2000);
+                        }
+                    },
+                    error: function (jqXHR, textStatus, err) {
+                        window.location.reload();
                     }
                 })
             }
--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const mdb = require("./configDB");
+const session = require("express-session");
+const cheerio = require("cheerio");
 
-let readData = "";
+let readData = "", retHead = "";
 
 app.use('/', express.static(path.join(__dirname, "../client")));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
@@ -12,37 +15,226 @@ app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/di
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/popper.js/dist/umd')));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font')));
+app.use(session({
+  secret: Date.now().toString(36) + Math.random().toString(36).substring(2),
+  saveUninitialized: false,
+  resave: false,
+  cookie: { maxAge: 86400000 }
+}));
 
-fs.readFile(path.join(__dirname, "../client/index.html"), "utf-8", (err, data) => {
+fs.readFile(path.join(__dirname, "../client/main.html"), "utf-8", (err, data) => {
   if (err) {
-    return console.log("Error");
+    return
   }
   readData = data;
 })
+fs.readFile(path.join(__dirname, "../client/header.html"), "utf-8", (err, data) => {
+  if (err) {
+    return
+  }
+  retHead = data;
+})
 
 app.get('/', (req, res) => {
-  res.send(readData);
+  if (req.session.authenticated) {
+    let temporaryHead = cheerio.load(retHead);
+    let temporaryData = cheerio.load(readData);
+    temporaryHead('body').find("#loginLink").replaceWith("<a class='nav-link' href='/cart' onclick='setActiveLink(this)' id='cart'><h2 style='color: gray;margin-right: 5vw;cursor: pointer;'><i class='bi bi-cart3'></i></h2></a></li><li class='nav-item'><a class='nav-link' href='#'><div class='dropdown'><h2 id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' style='color: gray;margin-right: 12vw;cursor: pointer;'> <i class='bi bi-person-circle'></i></h2><div class='dropdown-menu p-4' aria-labelledby='dropdownMenuButton' style='min-width: 375px;left: -175px;'><div class='card-body' style='border-bottom: 1px solid gray;'><div class='row'><div class='col'><h1 class='card-title'><i class='bi bi-person-circle'></i></h1></div><div class='col'><span id='spanName'>" + req.session.name + "</span><br><br><span id='spanEmail'>" + req.session.email + "</span></div></div></div><div class='card-footer text-center bg-transparent'><button type='button' class='btn btn-primary' onclick='window.location.href=`/logout`'>Logout</button></div></div></div></a>")
+    temporaryData("#headIF").replaceWith(temporaryHead.html());
+    res.send(temporaryData.html());
+  }
+  else {
+    res.send(readData);
+  }
 });
 
 app.get('/menu', (req, res) => {
-  let temp = readData.replace("./HomePage.html", "./MenuPage.html");
-  // temp = temp.replace("./assets/css/mainpage.css", "./assets/css/menupage.css");
-  temp = temp.replace(`id="first-page"`, `id="first-page" style="display:none"`)
-  res.send(temp);
+  let temp = cheerio.load(readData);
+  temp("#bodyIF").attr("src", "./MenuPage.html");
+  temp('#first-page').css("display", "none")
+  res.send(temp.html());
 });
 
 app.get('/login', (req, res) => {
-  let temp = readData.replace("./HomePage.html", "./LoginPage.html");
-  temp = temp.replace("./assets/css/mainpage.css", "./assets/css/login-register.css");
-  temp = temp.replace(`id="first-page"`, `id="first-page" style="display:none"`);
-  temp = temp.replace("./index.js", "./assets/js/validate.js");
-  res.send(temp);
+  if (req.session.authenticated) {
+    res.redirect("/");
+  }
+  else {
+    let temp = cheerio.load(readData);
+    temp("#bodyIF").attr("src", "./LoginPage.html");
+    temp('#first-page').css("display", "none")
+    temp("#mainCSS").attr("href", "./assets/css/login-register.css")
+    temp("#mainJS").attr("src", "./assets/js/validate.js")
+    res.send(temp.html());
+  }
+});
+
+app.get('/register', (req, res) => {
+  if (req.session.authenticated) {
+    res.redirect("/");
+  }
+  else {
+    let temp = cheerio.load(readData);
+    temp("#bodyIF").attr("src", "./RegisterPage.html");
+    temp('#first-page').css("display", "none")
+    temp("#mainCSS").attr("href", "./assets/css/login-register.css")
+    temp("#mainJS").attr("src", "./assets/js/validate.js")
+    res.send(temp.html());
+  }
+});
+
+app.get('/logout', (req, res) => {
+  if (req.session.authenticated) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(400);
+        res.send({ "response": "400" });
+      }
+      res.redirect("/");
+    })
+  }
+  else {
+    res.status(400);
+    res.send({ "response": "400" });
+  }
 });
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/login', urlencodedParser, (req, res) => {
-  console.log(req.body)
+app.post('/login', urlencodedParser, async (req, res) => {
+  if (req.session.authenticated) {
+    res.status(200);
+    res.send({ "response": 100 });
+    res.end();
+  }
+  else {
+    try {
+      let userData = req.body;
+      let email = userData["email"];
+      let emailParts = email.split("@");
+      if (emailParts.length > 2 || email.length < 4 || email.length > 64) {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      else if (emailParts[1] != "keshav.du.ac.in") {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      else {
+        let pwd = userData["password"];
+        try {
+          let checkExists = await mdb.checkUser(userData);
+          if (checkExists.flag === 0) {
+            res.status(200);
+            res.send({ "response": 404 });
+            res.end();
+          }
+          else {
+            if (checkExists.user["email"] === email && checkExists.user["password"] === pwd) {
+              req.session.authenticated = true;
+              req.session.email = checkExists.user["email"];
+              req.session.name = checkExists.user["fname"] + " " + checkExists.user["lname"];
+              res.status(200);
+              res.send({ "response": 200 });
+              res.end();
+            }
+            else {
+              res.status(200);
+              res.send({ "response": 500 });
+              res.end();
+            }
+          }
+        } catch (err) {
+          res.send({ "response": 500 });
+        }
+      }
+    } catch (e) {
+      res.send({ "response": 400 });
+    }
+  }
+});
+app.post('/register', urlencodedParser, async (req, res) => {
+  if (req.session.authenticated) {
+    res.status(200);
+    res.send({ "response": 100 });
+    res.end();
+  }
+  else {
+    try {
+      let userData = req.body;
+      let fname = userData["fname"];
+      let lname = userData["lname"];
+      let fnameTest = /^(?=.*[A-Za-z])[A-Za-z]{3,64}$/
+      let lnameTest = /^(?=.*[A-Za-z])[A-Za-z]{3,64}$/
+      if (fnameTest.test(fname) === false) {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      if (lname.length > 0 && lnameTest.test(lname) === false) {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      let email = userData["email"];
+      let emailParts = email.split("@");
+      if (emailParts.length > 2 || email.length < 4 || email.length > 64) {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      else if (emailParts[1] != "keshav.du.ac.in") {
+        res.send({ "response": 400 });
+        res.end();
+        return;
+      }
+      else {
+        let pwd = userData["password"];
+        let cpwd = userData["cpassword"];
+        let reg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d@*_]{7,19}$/;
+        if (reg.test(pwd) === false) {
+          res.send({ "response": 400 });
+          res.end();
+          return;
+        }
+        else if (pwd !== cpwd) {
+          res.send({ "response": 400 });
+          res.end();
+          return;
+        }
+        else {
+          try {
+            let checkExists = await mdb.checkUser(userData);
+            if (checkExists.flag === 0) {
+              let response = await mdb.addUser(userData);
+              if (response === 1) {
+                res.status(200);
+                res.send({ "response": 200 });
+                res.end();
+              }
+              else {
+                res.status(200);
+                res.send({ "response": 500 });
+                res.end();
+              }
+            }
+            else {
+              res.status(200);
+              res.send({ "response": 1 });
+              res.end();
+            }
+          } catch (err) {
+            res.send({ "response": 500 });
+          }
+        }
+
+      }
+    } catch (e) {
+      res.send({ "response": 400 });
+    }
+  }
 });
 
 app.get('/support', (req, res) => {
@@ -55,19 +247,11 @@ app.get('/feedback', (req, res) => {
   res.send(temp);
 });
 
-app.get('/register', (req, res) => {
-  let temp = readData.replace("./HomePage.html", "./RegisterPage.html");
-  temp = temp.replace("./assets/css/mainpage.css", "./assets/css/login-register.css");
-  temp = temp.replace(`id="first-page"`, `id="first-page" style="display:none"`);
-  temp = temp.replace("./index.js", "./assets/js/validate.js");
-  res.send(temp);
-});
-
 app.get('/about', (req, res) => {
   let temp = readData.replace("./HomePage.html", "./AboutPage.html");
   res.send(temp);
 });
 
 app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT || 3000}`);
+  // console.log(`Example app listening at http://localhost:${process.env.PORT || 3000}`);
 });
