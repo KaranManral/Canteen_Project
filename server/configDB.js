8cbe999a4b8a2ed862067855cebd1c7ecce0f291
KaranManral <karanmnaral2000@gmail.com>
2023-07-20 00:09:07 +0530

    env file added

--- server/configDB.js
+++ server/configDB.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://manga_deathNote:Hero9680@canteenproject.cxybxng.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.HOST;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const addUser = async (obj) => {
